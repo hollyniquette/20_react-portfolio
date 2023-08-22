b6986feb1209bc34d18b7ce584a2cac35a50849f
hollyniquette <127768285+hollyniquette@users.noreply.github.com>
2023-08-22 01:33:20 -0500

    more vite config changes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/",
-  };
-
-  if (command !== "serve") {
-    config.base = "/20_react-portfolio/";
-  }
-
-  return config;
+export default defineConfig({
+  plugins: [react()],
 });
