90a975dc8f79d3902a61b4fad0e361da188e75d9
hollyniquette <127768285+hollyniquette@users.noreply.github.com>
2023-08-22 02:05:56 -0500

    this doesn't make any sense

--- src/App.jsx
+++ src/App.jsx
@@ -10,33 +10,22 @@ import BiteBuddy from "./components/Projects/BiteBuddy";
 import WeatherDashboard from "./components/Projects/WeatherDashboard";
 import TextEditor from "./components/Projects/TextEditor";
 
-const BASE = "/20_react-portfolio";
-
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/20_react-portfolio/About" element={<About />} />
-        <Route path="/20_react-portfolio/projects" element={<Projects />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/soundvoter" element={<SoundVoter />} />
+        <Route path="/projects/text-editor" element={<TextEditor />} />
+        <Route path="/projects/bite-buddy" element={<BiteBuddy />} />
         <Route
-          path="/20_react-portfolio/projects/soundvoter"
-          element={<SoundVoter />}
-        />
-        <Route
-          path="/20_react-portfolio/projects/text-editor"
-          element={<TextEditor />}
-        />
-        <Route
-          path="/20_react-portfolio/projects/bite-buddy"
-          element={<BiteBuddy />}
-        />
-        <Route
-          path="/20_react-portfolio/projects/weather-dashboard"
+          path="/projects/weather-dashboard"
           element={<WeatherDashboard />}
         />
-        <Route path="/20_react-portfolio/Contact" element={<Contact />} />
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
     </div>
   );
