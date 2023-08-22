7affd9e7e3344a71ef9f82dcbb327571e10ff815
hollyniquette <127768285+hollyniquette@users.noreply.github.com>
2023-08-22 02:59:50 -0500

    HASHTAGS

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,16 +7,16 @@ function Projects() {
     <div id="projects" className="max-w-[1600px] m-auto mt-20 p-8 py-28">
       <div className="grid flex-auto sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 py-12">
         {/* TO DO: ADD PROJECT DESCRIPTIONS & LINKS */}
-        <a href="projects/soundvoter">
+        <a href="#/projects/soundvoter">
           <ProjectCard title="SoundVoter" />
         </a>
-        <a href="projects/text-editor">
+        <a href="#/projects/text-editor">
           <ProjectCard title="text editor" />
         </a>
-        <a href="projects/bite-buddy">
+        <a href="#/projects/bite-buddy">
           <ProjectCard title="bite buddy" />
         </a>
-        <a href="projects/weather-dashboard">
+        <a href="#/projects/weather-dashboard">
           <ProjectCard title="weather dashboard" />
         </a>
       </div>
