3d6bf60b5c28430eee05c6bb81f1ce89d3e804db
hollyniquette <127768285+hollyniquette@users.noreply.github.com>
2023-08-21 23:53:12 -0500

    finished populating project pages/links

--- src/components/Projects/SoundVoter.jsx
+++ src/components/Projects/SoundVoter.jsx
@@ -3,17 +3,35 @@ import soundvoterImg from "../../assets/soundvoter.gif";
 
 function SoundVoter() {
   return (
-    <div id="soundvoter" className="max-w-[1040px] m-auto p-4 py-28">
+    <div id="soundvoter" className="max-w-screen-xl m-auto p-8 mt-5 py-28">
       <h1 className="text-4xl font-bold text-center text-[#001b5e] pb-12">
         SoundVoter
       </h1>
-      <div className="grid sm:grid-cols-2 gap-12 py-8">
-        <img src={soundvoterImg} className="max-w-[600px]" />
-        <p className="">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi unde ex
-          quod pariatur, dolores incidunt eos, ratione ad dolorem hic ipsum
-          officiis veritatis vitae libero non eaque eligendi est nisi.
-        </p>
+      <div className="flex flex-col md:flex-row">
+        <div className="md:w-2/3 md:pr-10">
+          <img src={soundvoterImg} className="max-w-full" alt="Sound Voter" />
+        </div>
+        <div className="md:w-1/3 md:pr-10 mt-12">
+          <p className="text-lg">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi unde
+            ex quod pariatur, dolores incidunt eos, ratione ad dolorem hic ipsum
+            officiis veritatis vitae libero non eaque eligendi est nisi.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-row items-center space-x-4 justify-center mt-12">
+        <a
+          href="https://hollyniquette.github.io/sound-voter/"
+          className="bg-blue-600 text-white text-lg px-8 py-3 rounded hover:bg-blue-800 transition"
+        >
+          demo
+        </a>
+        <a
+          href="https://github.com/hollyniquette/sound-voter"
+          className="bg-blue-600 text-white text-lg px-8 py-3 rounded hover:bg-blue-800 transition"
+        >
+          repo
+        </a>
       </div>
     </div>
   );
